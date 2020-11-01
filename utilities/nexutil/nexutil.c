7ac18bfd8be866014b5b79ddc74076126237ce75
Simon Kaufmann <simon.k1996@gmail.com>
2020-11-01 20:06:26 +0000

    Compile simple timer program

--- utilities/nexutil/nexutil.c
+++ utilities/nexutil/nexutil.c
@@ -66,6 +66,10 @@
 #include <b64.h>
 #include <stdint.h>
 
+#include <stdio.h>
+#include <time.h>
+#include <signal.h>
+
 #define NEX_SKAUFMANN_ECW 550
 
 #define BUFFER_SIZE 34
@@ -140,8 +144,49 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
 
 static struct argp argp = {options, parse_opt, 0, doc};
 
+timer_t gTimerid;
+
+void start_timer(void)
+{
+    struct itimerspec value;
+
+    value.it_value.tv_sec = 1;
+    value.it_value.tv_nsec = 0;
+
+    value.it_interval.tv_sec = 1;
+    value.it_interval.tv_nsec = 0;
+
+    timer_create (CLOCK_REALTIME, NULL, &gTimerid);
+
+    timer_settime (gTimerid, 0, &value, NULL);
+}
+
+void stop_timer(void)
+{
+    struct itimerspec value;
+
+    value.it_value.tv_sec = 0;
+    value.it_value.tv_nsec = 0;
+
+    value.it_interval.tv_sec = 0;
+    value.it_interval.tv_nsec = 0;
+
+    timer_settime (gTimerid, 0, &value, NULL);
+}
+
+void timer_callback(int sig) {
+    printf(" Catched timer signal: %d ... !!\n", sig);
+}
+
 int main(int argc, char **argv)
 {
+    (void) signal(SIGALRM, timer_callback);
+    start_timer();
+    while(1);
+
+
+
+
     argp_parse(&argp, argc, argv, 0, 0, 0);
 
     struct nexio *nexio;
